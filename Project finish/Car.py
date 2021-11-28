bd2b00b322cde9a29b83c342ea0dd5cd7e68aa4f
Rustam Ibragimov <rustaibragimov@yandex.ru>
2021-11-28 19:07:46 +0300

    #Add def down #Add def up #Add def left #Add def right

--- Project finish/Car.py
+++ Project finish/Car.py
@@ -149,19 +149,37 @@ class Car:
         else:
             print("Включи мотор!")
 
-    # def up(self,steps):
-    #     if self.motor == True:
-    #         #Проверка фар взависимоти от времени суток
-    #         if (self.Times_of_day == False and self.light == True) or (self.Times_of_day == True):
-    #             #Есть ли дорога в том месте куда мы хотит передвигаться
-    #             if self.map[self.home[0]][self.home[1] + steps] == 1:
-    #                 #Не выходим ли мы за границы карты
-    #             else:
-    #                 print("Нет места")
-    #         else:
-    #             print("Включи фары")
-    #     else:
-    #         print("Включи мотор!")
+    def up(self,steps):
+        if self.motor == True:
+            #Проверка фар взависимоти от времени суток
+            if (self.Times_of_day == False and self.light == True) or (self.Times_of_day == True):
+                #Есть ли дорога в том месте куда мы хотит передвигаться
+                if self.map[self.home[0] - steps][self.home[1]] == 1:
+                    #Не выходим ли мы за границы карты
+                    if self.map[self.home[0] - steps][self.home[1]] < len(self.map[0]):
+                        # Сложение элемнтов от позиции звезды до новой позиции по строкам в переменную summ
+                        summka = 0
+                        for i in range(self.home[0] - steps,self.home[0]):
+                            summka = summka + self.map[i][self.home[1]]
+                            print(self.map[i][self.home[1]])
+
+                        if summka == steps:
+                            self.map[self.home[0]][self.home[1]] = 1
+                            self.home[0] = self.home[0] - steps
+                            self.map[self.home[0]][self.home[1]] = "*"
+                            for i in self.map:
+                                print(*i)
+                            print("down end")
+                        else:
+                            print("на пути препятствие")
+                    else:
+                        print(f"есть препятствие в точке куда хотим двигаться из точки {self.home[0]},{self.home[1]}")
+                else:
+                    print("Нет места")
+            else:
+                print("Включи фары")
+        else:
+            print("Включи мотор!")
 
 
     def left(self, steps):
@@ -178,10 +196,8 @@ class Car:
                         которые мы  хотим пройти
                         Элементы для суммирования берём с помощью среза self.home[1]-1:self.home[1]-1-steps
                         '''
-                        #sum(self.map[self.home[0]][self.home[1] + 1:self.home[1] + 1 + steps])
-                        #rowIndex = self.home[0]
-                        #colIndex = self.home[1] - 1
-                        _array_for_sum = self.map[self.home[0]][self.home[1]-1:self.home[1] + 1 -steps]
+                        _array_for_sum = self.map[self.home[0]][self.home[1] - steps:self.home[1]]
+                        print(_array_for_sum)
                         if sum(_array_for_sum) == steps:
                             self.map[self.home[0]][self.home[1]] = 1
                             self.home[1] = self.home[1] - steps
@@ -201,12 +217,20 @@ class Car:
             print("Включи мотор!")
 car = Car()
 print(car.cities())
-print(car.choose_city("Moscow"))
+for i in car.choose_city("Moscow"):
+    print(*i)
 print(car.home)
 car.open_close()
 car.start()
-car.down(2)
-car.right(1)
-car.down(2)
+car.down(1)
 car.left(1)
-
+car.down(2)
+car.left(2)
+car.right(2)
+car.up(2)
+car.right(2)
+car.down(4)
+car.Times_of_day = False
+car.light_on()
+car.left(3)
+car.down(1)
